34ede648ba5a81689499790d435b0ab345327105
Eric Gagnon <ergagnon@gmail.com>
2023-10-25 19:34:55 -0400

    feat: create base cli (#1)

---
+++ cmd/ginder_cli.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ergagnon/ginder/cmd/internal"
+	"github.com/ergagnon/gocmder"
+)
+
+func main() {
+	cli, err := gocmder.NewCmder(internal.AppConfig{}, func(cfg any) {
+			app := internal.NewApp(cfg.(internal.AppConfig))
+			app.Run()
+		},
+		gocmder.WithPrefix("GINDER"),
+		gocmder.WithLongDesc("\nGINDER help you find your most sensitive informations.\n"),
+		gocmder.WithVersion("0.0.1"),
+	)
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if err := cli.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ cmd/internal/config.go
@@ -0,0 +1,5 @@
+package internal
+
+type AppConfig struct {
+    Message string `desc:"Message to show in the console" default:"Hello world !!!"`
+}
